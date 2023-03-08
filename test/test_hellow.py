e23b406e811b322321e629bf813cf1c1aaed440a
NikNazarov <69785187+NikNazarov@users.noreply.github.com>
2023-03-08 19:21:35 +0700

    Update test_hellow.py

--- test/test_hellow.py
+++ test/test_hellow.py
@@ -3,6 +3,6 @@ from utils.widgets import make_wid
 
 def test_widg():
     assert make_wid() == "Hellow World!"
-def test_():
-    assert make_wid() == "Hellow!"
-    assert make_wid() == "Hellow !"
+# def test_():
+#     assert make_wid() == "Hellow!"
+#     assert make_wid() == "Hellow !"
